a999c3a3c69239f655200d4eba9e4d4a915c8fea
MarshallEvergreen <abiemarshall94@gmail.com>
2021-07-27 18:18:25 +0100

    Started to implement chakra ui

---
+++ src/components/navbar.js
@@ -0,0 +1,56 @@
+import * as React from "react"
+import {
+    Box,
+    Flex,
+    Avatar,
+    HStack,
+    Link,
+    IconButton,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+    useDisclosure,
+    useColorModeValue,
+    Stack,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+const Links = ['Dashboard', 'Projects', 'Team'];
+
+const NavLink = ({ children }) => (
+    <Link
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+            textDecoration: 'none',
+            bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={children.toLowerCase()}>
+        {children}
+    </Link>
+);
+
+export default function Simple() {
+    return (
+        <>
+            <Box alignItems="center" bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+                <Flex alignItems="center" h={16} justifyContent={'right'}>
+                    <HStack alignItems="center" spacing={8}>
+                        <HStack
+                            as={'nav'}
+                            spacing={4}
+                            display={{ base: 'none', md: 'flex' }}>
+                            {Links.map((link) => (
+                                <NavLink key={link}>{link}</NavLink>
+                            ))}
+                        </HStack>
+                    </HStack>
+                </Flex>
+            </Box>
+        </>
+    );
+}
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,16 +1,13 @@
 import * as React from "react"
 import { ChakraProvider } from "@chakra-ui/react"
-import SocialProfileWithImage from "../components/myCard";
 import WithSubnavigation from "../components/navbar";
-import ProductSimple from "../components/myCard";
 
-const IndexPage = () => {
+const Dashboard = () => {
     return <div>
         <ChakraProvider>
             <WithSubnavigation/>
-            <ProductSimple/>
         </ChakraProvider>
     </div>
 }
 
-export default IndexPage
+export default Dashboard
