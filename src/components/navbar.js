df59d93d2616a48160d8ecb20449a55ab24d3f98
MarshallEvergreen <abiemarshall94@gmail.com>
2021-08-04 18:22:00 +0100

    Added transitions and return button to single site page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,5 @@
 import * as React from "react"
-import { Link as GatsbyLink } from "gatsby"
-import { Link as ScrollLink } from "react-scroll";
+import Scroll, { Link as ScrollLink } from "react-scroll";
 import logo from '../images/logo.svg';
 import {
     Box,
@@ -16,13 +15,31 @@ import {HamburgerIcon, CloseIcon} from '@chakra-ui/icons';
 
 const Links = ['Education', 'Work'];
 
-const NavLink = ({ label, link }) => (
+const scroll = Scroll.animateScroll;
+
+const HomeLink = () => (
+    <HStack spacing={8}>
+        <Image
+            onClick={() => scroll.scrollToTop()}
+            cursor={'pointer'}
+            src={logo}
+            minW={'40px'}
+            maxW={'80px'}
+            height={'auto'}
+        />
+    </HStack>
+)
+
+export default function Navbar() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const NavLink = ({ label, link }) => (
         <ScrollLink
             to={link}
             spy={true}
             smooth={true}
             duration={500}>
             <Text
+                onClick={isOpen ? onClose : onOpen}
                 px={2}
                 py={1}
                 rounded={'md'}
@@ -34,27 +51,13 @@ const NavLink = ({ label, link }) => (
             >{label}
             </Text>
         </ScrollLink>
-)
-
-const HomeLink = () => (
-    <HStack spacing={8}>
-        <GatsbyLink to={"/#/"}>
-            <Image
-                src={logo}
-                minW={'40px'}
-                maxW={'80px'}
-                height={'auto'}
-            />
-        </GatsbyLink>
-    </HStack>
-)
-
-export default function Navbar() {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-
+    )
     return (
         <>
             <Box
+                position={'fixed'}
+                width={'100%'}
+                zIndex={10}
                 borderBottomWidth={1}
                 borderStyle={'solid'}
                 bg={useColorModeValue('white', 'gray.900')} px={4}>
@@ -84,7 +87,8 @@ export default function Navbar() {
                     <Box pb={4} display={{ md: 'none' }}>
                         <Stack as={'nav'} spacing={4}>
                             {Links.map((link) => (
-                                <NavLink label={link} link={link}/>
+                                <NavLink
+                                    label={link} link={link}/>
                             ))}
                         </Stack>
                     </Box>
--- src/pages/page.js
+++ src/pages/page.js
@@ -1,13 +1,49 @@
 import * as React from "react"
-import {Center, ChakraProvider, Flex, Stack, useColorModeValue} from "@chakra-ui/react"
+import {Center, ChakraProvider, Collapse, Fade, Flex, ScaleFade, useColorModeValue} from "@chakra-ui/react"
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
+import {useEffect, useState} from "react";
 
 const Page = ({children}) => {
+    const [scrollDir, setScrollDir] = useState("scrolling up");
+    useEffect(() => {
+        const threshold = 75;
+        let lastScrollY = window.pageYOffset;
+        let ticking = false;
+
+        const updateScrollDir = () => {
+            const scrollY = window.pageYOffset;
+
+            if (Math.abs(scrollY - lastScrollY) < threshold) {
+                ticking = false;
+                return;
+            }
+            setScrollDir(scrollY > lastScrollY ? "scrolling down" : "scrolling up");
+            lastScrollY = scrollY > 0 ? scrollY : 0;
+            ticking = false;
+        };
+
+        const onScroll = () => {
+            if (!ticking) {
+                window.requestAnimationFrame(updateScrollDir);
+                ticking = true;
+            }
+        };
+
+        window.addEventListener("scroll", onScroll);
+
+        console.log(scrollDir);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [scrollDir]);
     return <Flex minH={'98vh'} flexDirection={'column'}>
         <ChakraProvider>
-            <Navbar/>
-            <Center flexGrow={1}
+            <Collapse in={scrollDir === 'scrolling up'}>
+                <Navbar/>
+            </Collapse>
+            <Center
+                paddingTop={{base: '100px', lg: '60px'}}
+                flexGrow={1}
                 bg={useColorModeValue('purple.50', 'gray.900')}
                 color={useColorModeValue('gray.700', 'gray.200')}>
                 {children}
