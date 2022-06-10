26752a82cef0c23508e9ac69285212cf7a7b3e66
Abie Marshall <abiemarshall94@gmail.com>
2022-06-10 17:45:52 +0100

    Updates

---
+++ 252f366e-fe10c0d843df7962fed6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_website=self.webpackChunkpersonal_website||[]).push([[228],{2775:function(t,e,n){n.d(e,{ZSR:function(){return r}});var a=n(1046);function r(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-fe10c0d843df7962fed6.js.map
