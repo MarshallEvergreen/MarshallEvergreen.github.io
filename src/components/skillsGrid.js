97e053cc127834a299c1510130e74c84826316e0
Abie Marshall <abiemarshall94@gmail.com>
2025-05-17 17:15:26 +0100

    updated skills grid

--- src/components/skillsGrid.js
+++ src/components/skillsGrid.js
@@ -52,39 +52,51 @@ export default function SkillsGrid() {
             <SkillGrid
                 heading={'Some of my favourite tech to work with is...'}>
                 <SkillIcon icon={'cplusplus-plain'} link={'https://isocpp.org/'} />
+                <SkillIcon icon={'rust-plain'} link={'https://www.rust-lang.org/'} />
                 <SkillIcon icon={'python-plain-wordmark'} link={'https://www.python.org/'} />
-                <SkillIcon icon={'jupyter-plain-wordmark'} link={'https://jupyter.org/'} />
-                <SkillIcon icon={'lua-plain-wordmark'} link={'https://www.lua.org/'} />
+                <SkillIcon icon={'grpc-plain'} link={'https://grpc.io/'} />
                 <SkillIcon icon={'docker-plain-wordmark'} link={'https://www.docker.com/'} />
                 <SkillIcon icon={'react-plain-wordmark'} link={'https://reactjs.org/'} />
             </SkillGrid>
             <SkillGrid
                 heading={"I've worked with a few other things as well..."}>
                 <SkillIcon icon={'csharp-plain'} link={'https://docs.microsoft.com/en-us/dotnet/csharp/'} />
-                <SkillIcon icon={'matlab-plain'} link={'https://www.mathworks.com/products/matlab.html'} />
-                <SkillIcon icon={'javascript-plain'} link={'https://www.javascript.com/'} />
                 <SkillIcon icon={'typescript-plain'} link={'https://www.typescriptlang.org/'} />
-                <SkillIcon icon={'mongodb-plain-wordmark'} link={'https://www.mongodb.com/'} />
+                <SkillIcon icon={'lua-plain-wordmark'} link={'https://www.lua.org/'} />
+                <SkillIcon icon={'pytorch-original'} link={'https://pytorch.org/'} />
                 <SkillIcon icon={'angularjs-plain'} link={'https://angular.io/'} />
-                <SkillIcon icon={'rust-plain'} link={'https://www.rust-lang.org/'} />
-                <SkillIcon icon={'go-plain'} link={'https://golang.org/'} />
+                <SkillIcon icon={'fastify-plain'} link={'https://fastify.dev/'} />
+                <SkillIcon icon={'fastapi-plain'} link={'https://fastapi.tiangolo.com/'} />
                 <SkillIcon icon={'cucumber-plain'} link={'https://cucumber.io/'} />
-                <SkillIcon icon={'gradle-plain'} link={'https://gradle.org/'} />
-                <SkillIcon icon={'kotlin-plain'} link={'https://kotlinlang.org/'} />
-                <SkillIcon icon={'flask-plain'} link={'https://flask.palletsprojects.com/en/2.0.x/'} />
+                <SkillIcon icon={'selenium-original'} link={'https://www.selenium.dev/'} />
+                <SkillIcon icon={'postgresql-plain'} link={'https://www.postgresql.org/'} />
+                <SkillIcon icon={'sqlalchemy-plain'} link={'https://www.sqlalchemy.org/'} />
+                <SkillIcon icon={'prisma-plain'} link={'https://www.prisma.io/'} />
+                {/* <SkillIcon icon={'gradle-plain'} link={'https://gradle.org/'} /> */}
+                {/* <SkillIcon icon={'kotlin-plain'} link={'https://kotlinlang.org/'} /> */}
+                {/* <SkillIcon icon={'flask-plain'} link={'https://flask.palletsprojects.com/en/2.0.x/'} /> */}
             </SkillGrid>
             <SkillGrid
                 heading={'Tools I\'ve used to build awesome stuff include...'}>
                 <SkillIcon icon={'git-plain-wordmark'} link={'https://git-scm.com/'} />
+                <SkillIcon icon={'github-original-wordmark'} link={'https://github.com/'} />
                 <SkillIcon icon={'bitbucket-plain-wordmark'} link={'https://bitbucket.org/product'} />
-                <SkillIcon icon={'github-plain'} link={'https://github.com/'} />
+                <SkillIcon icon={'jira-plain-wordmark'} link={'https://www.atlassian.com/software/jira'} />
                 <SkillIcon icon={'jetbrains-plain'} link={'https://www.jetbrains.com/'} />
-                <SkillIcon icon={'visualstudio-plain'} link={'https://visualstudio.microsoft.com/'} />
+                {/* <SkillIcon icon={'visualstudio-plain'} link={'https://visualstudio.microsoft.com/'} /> */}
                 <SkillIcon icon={'vscode-plain'} link={'https://code.visualstudio.com/'} />
-                <SkillIcon icon={'ubuntu-plain'} link={'https://ubuntu.com/'} />
-                <SkillIcon icon={'windows8-plain'}
-                    link={'https://www.microsoft.com/en-gb/software-download/windows10ISO'} />
-                <SkillIcon icon={'gatsby-plain'} link={'https://www.gatsbyjs.com/'} />
+            </SkillGrid>
+            <SkillGrid
+                heading={'And I\'ve then deployed and orchestrated them with...'}>
+                <SkillIcon icon={'amazonwebservices-plain-wordmark'} link={'https://aws.amazon.com/'} />
+                <SkillIcon icon={'docker-plain-wordmark'} link={'https://www.docker.com/'} />
+                <SkillIcon icon={'kubernetes-plain'} link={'https://kubernetes.io/'} />
+                <SkillIcon icon={'bamboo-original'} link={'https://www.atlassian.com/software/bamboo'} />
+                <SkillIcon icon={'circleci-plain'} link={'https://circleci.com/'} />
+                <SkillIcon icon={'githubactions-plain'} link={'https://github.com/features/actions'} />
+                <SkillIcon icon={'apacheairflow-plain'} link={'https://airflow.apache.org/'} />
+                <SkillIcon icon={'swagger-plain'} link={'https://swagger.io/'} />
+                <SkillIcon icon={'pypi-plain'} link={'https://github.com/features/actions'} />
             </SkillGrid>
         </Accordion>
     )
